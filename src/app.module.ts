df35cdb466a5ded38a7d69b034a9ba34ff51a2f1
mohamed 3rafa <mohamed3rafa01@gmail.com>
2026-08-06 23:22:48 +0300

    setup: integrate Prisma ORM with NestJS
    - Clean up default comments in Prisma schema
    - Create PrismaService to manage database connection lifecycle
    - Create PrismaModule and export PrismaService
    - Register PrismaModule and PrismaService in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
